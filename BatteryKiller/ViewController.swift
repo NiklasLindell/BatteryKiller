3976a843f59a0b2802f7eb364ebbfbcdd18677d2
Niklas Lindell <i-nili@i-nili-mbp15-1901.local>
2019-01-29 12:25:45 +0100

    Initial Commit

---
+++ BatteryKiller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BatteryKiller
+//
+//  Created by Niklas Lindell on 2019-01-29.
+//  Copyright © 2019 Niklas Lindell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
