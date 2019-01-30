3e629c42d8fe353537ba388b7b83b7f00f41b03b
Matilda Dahlberg <i-mada@anton-mbp13-1804.local>
2019-01-30 09:50:15 +0100

    functions to see battery info

--- BatteryKiller/ViewController.swift
+++ BatteryKiller/ViewController.swift
@@ -9,22 +9,35 @@ class ViewController: UIViewController {
     var soundTimer : Timer!
     var torchTimer : Timer!
     
+    var batteryLevel : Float{
+        return UIDevice.current.batteryLevel
+    }
+    
+    var batteryState: UIDevice.BatteryState {
+        return UIDevice.current.batteryState
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        UIDevice.current.isBatteryMonitoringEnabled = true
         
+         NotificationCenter.default.addObserver(self, selector: #selector(batteryLevelDidChange), name: UIDevice.batteryLevelDidChangeNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(batteryStateDidChange), name: UIDevice.batteryStateDidChangeNotification, object: nil)
+
         
         vibrationTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(startVibration), userInfo: nil, repeats: true)
-        
+
         startFlashBackground()
-        
-        
-        
+
+
+
         soundTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(startSound), userInfo: nil, repeats: true)
-        
-        
-        
+
+
+
         torchTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(startTorch), userInfo: nil, repeats: true)
         
         
@@ -66,12 +79,14 @@ class ViewController: UIViewController {
         }
     }
     
+    //func to start sound
     @objc func startSound(){
         AudioServicesPlaySystemSound(1005)
         print("SOUND")
         
     }
     
+    //func to start flash background
     func startFlashBackground(){
         UIView.animate(withDuration: 0.1, delay: 0.0, options:[UIView.AnimationOptions.repeat, UIView.AnimationOptions.autoreverse], animations: {
             self.view.backgroundColor = UIColor.yellow
@@ -80,6 +95,23 @@ class ViewController: UIViewController {
             //self.view.backgroundColor = UIColor.red
         }, completion: nil)
         print("BACKGROUND")
+    }
+    
+    
+    //func to see batterylevel
+    @objc func batteryLevelDidChange(_ notification: Notification) {
+        print(batteryLevel)
+        print("battery level")
+    }
+    
+    //func to see battery state
+    @objc func batteryStateDidChange(_ notification: Notification) {
+        switch batteryState {
+        case .unplugged, .unknown:
+            print("not charging")
+        case .charging, .full:
+            print("charging or full")
+        }
     }
 }
 
