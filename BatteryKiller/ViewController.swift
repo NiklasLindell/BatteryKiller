61d8976440e4bf36f2c12e4fb6184df7a0f7e01a
Niklas Lindell <i-nili@i-nili-mbp15-1901.local>
2019-01-30 10:51:59 +0100

    added views on storyboard

--- BatteryKiller/ViewController.swift
+++ BatteryKiller/ViewController.swift
@@ -4,10 +4,16 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var textView: UITextView!
     
     var vibrationTimer : Timer!
     var soundTimer : Timer!
     var torchTimer : Timer!
+    
+    let battery = UIDevice.current.batteryLevel
+    var batteryShow : Timer!
     
     var batteryLevel : Float{
         return UIDevice.current.batteryLevel
@@ -20,27 +26,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        UIScreen.main.brightness = CGFloat(1.0)
+        
         
         UIDevice.current.isBatteryMonitoringEnabled = true
         
          NotificationCenter.default.addObserver(self, selector: #selector(batteryLevelDidChange), name: UIDevice.batteryLevelDidChangeNotification, object: nil)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(batteryStateDidChange), name: UIDevice.batteryStateDidChangeNotification, object: nil)
+//        NotificationCenter.default.addObserver(self, selector: #selector(batteryStateDidChange), name: UIDevice.batteryStateDidChangeNotification, object: nil)
 
         
         vibrationTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(startVibration), userInfo: nil, repeats: true)
-
         startFlashBackground()
-
-
-
-        soundTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(startSound), userInfo: nil, repeats: true)
-
-
-
+        //soundTimer = Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(startSound), userInfo: nil, repeats: true)
         torchTimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(startTorch), userInfo: nil, repeats: true)
+        batteryShow = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(startBattery), userInfo: nil, repeats: true)
         
-        
+    }
+    
+    @objc func startBattery(){
+        print(batteryLevel)
+        let label = UILabel(frame: CGRect(x: 100, y: 100, width: 200, height: 100))
+        label.text = "BATTERY: \(batteryLevel*100)%"
+        self.view.addSubview(label)
     }
     
     
